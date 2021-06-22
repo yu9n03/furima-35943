f49dcfadb7ef707a8ab71d1efee6603580771b18
yu9n03 <yu9n03@gmail.com>
2021-06-22 19:13:57 +0900

    新規登録のテストコードの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 4)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
     last_name             {Gimei.last.kanji}
     first_name            {Gimei.first.kanji}
