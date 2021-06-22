fdb2fe22e09028ede28a3a5f833ca8300c47ccc0
yu9n03 <yu9n03@gmail.com>
2021-06-22 17:53:16 +0900

    Gimeiの導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-
+    nickname              {Faker::Name.initials(number: 4)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {Gimei.last.kanji}
+    first_name            {Gimei.first.kanji}
+    last_name_kana        {Gimei.last.katakana}
+    first_name_kana       {Gimei.first.katakana}
+    birthday              {Faker::Date.between(from: '1930-01-01', to: '2016-12-31')}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録/ユーザー情報' do
+    context '新規登録できるとき' do
+      it '' do
+      end
+    end
+    context '新規登録できないとき' do
+      it '' do
+      end
+    end
+  end
+
+  describe 'ユーザー新規登録/本人確認情報' do
+    context '新規登録できるとき' do
+      it '' do
+      end
+    end
+    context '新規登録できないとき' do
+      it '' do
+      end
+    end
+  end
 end
