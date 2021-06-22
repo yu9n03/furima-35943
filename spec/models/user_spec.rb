fa9f89f837abf206feafe62d2ffaa8514cd903fb
yu9n03 <yu9n03@gmail.com>
2021-06-22 18:10:07 +0900

    モデル単体テストのexampleの洗い出し

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,22 +7,50 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録/ユーザー情報' do
     context '新規登録できるとき' do
-      it '' do
+      it 'nickname、password、password_confirmation、last_name、first_name、last_name_kana、first_name_kana、birthday全てがあれば登録できる' do
+      end
+      it 'passwordとpassword_confirmationは6文字以上の半角英数混合の入力であれば登録できる' do
       end
     end
     context '新規登録できないとき' do
-      it '' do
+      it 'nicknameが空では登録できない' do
+      end
+      it 'emailが空では登録できない' do
+      end
+      it '重複したemailが存在する場合登録できない' do
+      end
+      it 'パスワードに@を含まないと登録できない' do
+      end
+      it 'passwordが空では登録できない' do
+      end
+      it 'passwordが5文字以下では登録できない' do
+      end
+      it 'passwordが半角英数混合でなければ登録できない' do
+      end
+      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
       end
     end
   end
 
   describe 'ユーザー新規登録/本人確認情報' do
-    context '新規登録できるとき' do
-      it '' do
-      end
-    end
     context '新規登録できないとき' do
-      it '' do
+      it 'last_nameが空では登録できない' do
+      end
+      it 'first_nameが空では登録できない' do
+      end
+      it 'last_name_kanaが空では登録できない' do
+      end
+      it 'first_name_kanaが空では登録できない' do
+      end
+      it 'birthdayが空では登録できない' do
+      end
+      it 'last_nameが全角（漢字・ひらがな・カタカナ）でなければ登録できない' do
+      end
+      it 'first_nameが全角（漢字・ひらがな・カタカナ）でなければ登録できない' do
+      end
+      it 'last_name_kanaが全角（カタカナ）でなければ登録できない' do
+      end
+      it 'first_nameが全角（カタカナ）でなければ登録できない' do
       end
     end
   end
