2f1d8d744eeabb66b6c081e161f40006469fe6c9
yu9n03 <yu9n03@gmail.com>
2021-06-23 10:50:19 +0900

    itemモデルにバリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-end
+
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :category_id, presence: true
+  validates :charge_rate_id, presence: true
+  validates :status_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :until_send_id, presence: true
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :images, presence: true
