99a87e28d366a8ba0c56025927b416eabe81bbcd
yu9n03 <yu9n03@gmail.com>
2021-06-25 11:34:05 +0900

    editアクションの不要な条件を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless user_signed_in? && current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
       redirect_to root_path
     end
   end
