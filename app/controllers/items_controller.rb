a90ef3c9e735007c7c239eb35c25914488521ff1
yu9n03 <yu9n03@gmail.com>
2021-06-25 09:34:06 +0900

    編集後の更新機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,6 +30,12 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
   end
 
   private
