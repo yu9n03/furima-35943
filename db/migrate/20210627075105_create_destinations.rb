8e9e675d48765cd345877f52ebbaebba2fe954ef
yu9n03 <yu9n03@gmail.com>
2021-06-29 17:31:37 +0900

    マイグレーションファイルの修正（building_name）

--- db/migrate/20210627075105_create_destinations.rb
+++ db/migrate/20210627075105_create_destinations.rb
@@ -5,7 +5,7 @@ class CreateDestinations < ActiveRecord::Migration[6.0]
       t.integer    :prefecture_id,    null: false
       t.string     :municipality,     null: false
       t.string     :address,          null: false
-      t.string     :building_name,    null: false
+      t.string     :building_name
       t.string     :phone_number,     null: false
       t.references :order,            null: false, foreign_key: true
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_06_27_075105) do
     t.integer "prefecture_id", null: false
     t.string "municipality", null: false
     t.string "address", null: false
-    t.string "building_name", null: false
+    t.string "building_name"
     t.string "phone_number", null: false
     t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
